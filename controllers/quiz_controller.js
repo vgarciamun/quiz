a89424b6b80ce37bfae035093f3dab6dd0e16878
vgarciamun <vgarcia@grecasa.org>
2015-07-19 10:16:15 +0100

    Despliegue de busqueda de preguntas en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,10 +14,18 @@ exports.load = function(req, res, next, quizId){
 
 //GET /quizes
 exports.index = function(req,res) {
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index', { quizes: quizes});
-	}
-	).catch(function(error) { next(error);});
+	var busqueda = ('%' + req.query.search + '%').replace(/ /g,'%');
+		if (req.query.search) {
+			models.Quiz.findAll({where: ["pregunta like ?", busqueda],
+								 order: ['pregunta']}
+								 ).then(function(quizes){
+						res.render('quizes/index', { quizes: quizes});
+		}).catch(function(error) {next(error);});
+		}else{
+			models.Quiz.findAll().then(function(quizes) {
+				res.render('quizes/index', { quizes: quizes});
+		}).catch(function(error) {next(error);});
+		}
 };
 
 // GET /quizes/question
